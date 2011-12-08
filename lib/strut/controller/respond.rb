a08a43d38bb12a57dc4e8d2487df9a4eed0ea1a9
kuruma3 <kuruma@galileoscope.com>
2011-12-08 13:02:39 +0900

    update

--- lib/strut/controller/respond.rb
+++ lib/strut/controller/respond.rb
@@ -73,7 +73,7 @@ module Strut
         options[:f_json] ||= Proc.new{ render json: obj.errors, status: :unprocessable_entity }
         options[:f_js]   ||= Proc.new{}
         respond_to do |format|
-          if obj.update_attributes(params[obj.class.to_s.underscore.gsub("/","_").to_sym])
+          if obj.update_attributes(params[file_name.to_sym])
             flash[:notice] = options[:t_message]
             options[:t].call
             format.html &options[:t_html]
